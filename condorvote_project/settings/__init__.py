a6d03072ce3c0424df6a3b8306ed16b271da9a10
Thibault231 <salgues231@gmail.com>
2020-07-27 14:15:58 +0200

    V 1.3.1 Add raven and sentry monitoring

--- condorvote_project/settings/__init__.py
+++ condorvote_project/settings/__init__.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from dotenv import load_dotenv
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
